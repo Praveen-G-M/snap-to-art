4926e21ee0034c7713b18fc30fd4545fde283ce7
Praveen-G-M <praveengmpravin@gmail.com>
2026-01-21 20:31:22 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/snap-to-art/'
+  base: "/snap-to-art",
 })
